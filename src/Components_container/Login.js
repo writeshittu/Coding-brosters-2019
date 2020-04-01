ba96522190f2da955180d9c4f8c55608dbd3a759
shit2boy <writeshittu@gmail.com>
2020-04-01 10:47:11 +0100

    ok

--- src/Components_container/Login.js
+++ src/Components_container/Login.js
@@ -3,7 +3,10 @@ import React from "react";
 const Login =()=>{
     return(
         <div>
-            INSERT CONTENTS !!!!!!!!!!!!!!!!!
+            <hr/>
+            <div>
+                INSERT CONTENTS !!!!!!!!!!!!!!!!!
+            </div>
         </div>
     )
 }
