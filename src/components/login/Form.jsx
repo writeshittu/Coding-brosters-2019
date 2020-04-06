568adc39020975f45be5b068f66b7509f7a68679
Sambalicious <stemitope370@gmail.com>
2020-04-06 12:56:36 +0100

    sorted the register form

--- src/components/login/Form.jsx
+++ src/components/login/Form.jsx
@@ -8,7 +8,7 @@ const Form = ({label,type, placeholder}) => {
         <div>
              
               <div className="w-full mb-6 ">
-                <label className="block text-gray-700 text-sm font-bold mb-2" for={label}>
+                <label className="block text-gray-700 text-sm font-bold mb-1" for={label}>
                    {label}
                  </label>
                   <input className="shadow appearance-none  w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline" id="username" type={type} placeholder={placeholder} />
