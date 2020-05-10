67aecd75d2dc8aca261905737c042667eeef59cf
David <drizzzydave@gmail.com>
2020-05-10 17:27:59 +0100

    Content added to the About Us page

--- src/components/login/RememberMe.jsx
+++ src/components/login/RememberMe.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import {Link } from 'react-router-dom'
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 const RememberMe = () => {
-    return ( 
-            <div  className="flex justify-between -mt-4 mb-10">
-                 <span className="text-sm" >
-                    <input type="checkbox" value="" />Remember Me
-                    </span>
-        <Link to='/password-Reset' className="text-red-500 text-sm">Forgot password</Link>
-        </div>
-     );
-}
- 
+  return (
+    <div className="flex justify-between -mt-4 mb-10">
+      <span className="text-sm">
+        <input type="checkbox" value="" />
+        Remember Me
+      </span>
+      <Link to="/password-Reset" className="text-red-500 text-sm">
+        Forgot password?
+      </Link>
+    </div>
+  );
+};
+
 export default RememberMe;
